c1ac04687469b14203ba547dcc657742cce3435f
hamster197 <hamster197@mail.ru>
2019-09-18 16:55:03 +0300

    fix

--- vk/forms.py
+++ vk/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class LoginForm(forms.Form):
-    lgn = forms.CharField(label='Введите логин VC:', max_length=20, required=True)
+    lgn = forms.CharField(label='Введите логин VC:', max_length=20, required=True, help_text='email или телефон')
     psw = forms.CharField(label='Пароль VC', max_length=200, widget=forms.PasswordInput, required=True)
